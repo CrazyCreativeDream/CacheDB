652206947b30f3f562bd7d2172dd7919f63a90ed
ChenYFan <chen2778754364@foxmail.com>
2022-07-06 09:38:16 +0000

    F First Upload

---
+++ README.md
@@ -0,0 +1,33 @@
+# Cache-DB
+
+A common front-end/Service Worker-based Key/Value database based on CacheStorage
+
+
+```md
+> const db = new CacheDB('ChenYFanDB')
+< undefined
+> await db.read('ChenYFan')
+< null
+> await db.write('ChenYFan','YYDS')
+< true
+> await db.read('ChenYFan')
+< 'YYDS'
+> await db.write('ChenYFan',new Blob([]))
+< true
+> await db.read('ChenYFan')
+< ''
+> await db.delete('ChenYFan')
+< true
+> await db.read('ChenYFan')
+< null
+```
+
+You can import this script as:
+
+```js
+import CacheDB from '@ChenYFan/cache-db' //ServiceWorker
+```
+
+```html
+<script src="https://unpkg.com/@ChenYFan/cache-db"></script>
+```
---
+++ index.js
@@ -0,0 +1,75 @@
+(function (factory) {
+    if (typeof exports === 'object') {
+        module.exports = factory();
+    } else if (typeof define === 'function' && define.amd) {
+        define(factory);
+    } else if (typeof window === 'object') {
+        window.CacheDB = factory();
+    } else {
+        self.CacheDB = factory();
+    }
+
+})(function () {
+    function CacheDB(namespace, prefix) {
+        this.namespace = namespace || "CacheDBDefaultNameSpace";
+        this.prefix = prefix || "CacheDBDefaultPrefix";
+        this.read = async function (key, config) {
+            config = config || { type: "text" };
+            return new Promise((resolve, reject) => {
+                caches.open(this.namespace).then(cache => {
+                    cache.match(new Request(`https://${this.prefix}/${encodeURIComponent(key)}`))
+                        .then(response => {
+                            switch (config.type) {
+                                case "json":
+                                    resolve(JSON.parse(response.body));
+                                case "arrayBuffer":
+                                    resolve(response.arrayBuffer());
+                                case 'blob':
+                                    resolve(response.blob());
+                                default:
+                                    resolve(response.text());
+                            }
+                        }).catch(err => { resolve(null); });
+                })
+            })
+        }
+        this.write = async function (key, value, config) {
+            config = config || { type: "text" };
+            return new Promise((resolve, reject) => {
+                caches.open(this.namespace).then(cache => {
+                    cache.put(
+                        new Request(`https://${this.prefix}/${encodeURIComponent(key)}`),
+                        new Response(value, {
+                            headers: {
+                                'Content-Type': (() => {
+                                    switch (config.type) {
+                                        case "json":
+                                            return 'application/json';
+                                        case "arrayBuffer":
+                                            return 'application/octet-stream';
+                                        case 'blob':
+                                            return 'application/octet-stream';
+                                        default:
+                                            return 'text/plain';
+                                    }
+                                })()
+                            }
+                        }))
+                        .then(resolve(true))
+                        .catch(err => { resolve(false) });
+                })
+            })
+        }
+        this.delete = async function (key) {
+            return new Promise((resolve, reject) => {
+                caches.open(this.namespace).then(cache => {
+                    cache.delete(new Request(`https://${this.prefix}/${encodeURIComponent(key)}`))
+                        .then(resolve(true))
+                        .catch(err => { resolve(false) });
+                })
+            })
+        }
+    }
+    return CacheDB;
+})
+
